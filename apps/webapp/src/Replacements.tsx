b2a9608eff2e4944d52a67835544788daaf85716
James Harris <computersherpa@gmail.com>
2025-06-12 22:28:16 -0400

    Typography improvements

--- apps/webapp/src/Replacements.tsx
+++ apps/webapp/src/Replacements.tsx
@@ -14,21 +14,19 @@ function renderReplacement(replacement: Replacement, idx: number) {
       className="col-span-2 mb-8 flex flex-col items-stretch px-2"
     >
       <div className="grid grid-cols-2 bg-white border border-gray-200 rounded-3xl shadow-lg px-8 py-6 items-center relative z-10" style={{ minHeight: '80px' }}>
-        <div className="text-orange-700 text-xs">ORIGINAL</div>
-        <div className="text-green-700 text-xs">REPLACEMENT</div>
+        <div className="text-xs pl-2 text-orange-700">ORIGINAL</div>
+        <div className="text-xs pl-2 text-green-700">REPLACEMENT</div>
         <div className="px-2 py-1 h-full text-left font-medium text-orange-700 bg-orange-100 rounded-l-md whitespace-pre-line break-words">
           {replacement.originalHeadline}
         </div>
         <div className="px-2 py-1 h-full font-semibold text-green-700 bg-green-200 rounded-r-md whitespace-pre-line break-words">
           {replacement.replacementHeadline}
         </div>
-      </div>
-      <div className="grid grid-cols-2 bg-gray-100 rounded-b-3xl px-8 pb-3 pt-2 -mt-2 border-t border-gray-200 relative z-0">
-        <div className="text-xs text-gray-500 col-span-1 break-all">
+        <div className="my-2 pl-2 text-sm text-gray-500 col-span-1 break-all">
           <a href={replacement.url} target="_blank" rel="noopener noreferrer" className="hover:underline">{replacement.url}</a>
         </div>
-        <div className="text-xs text-right text-gray-500 col-span-1">
-          <span className="inline-block rounded-full bg-yellow-100 text-yellow-800 px-3 py-1 font-semibold">{replacement.status}</span>
+        <div className="flex justify-end">
+          <span className="inline-block rounded-full bg-yellow-100 text-yellow-800 px-3 py-1 font-semibold text-sm shrink">{replacement.status}</span>
         </div>
       </div>
     </div>
@@ -63,7 +61,7 @@ export default function Replacements() {
     },
     {
       originalHeadline: "City Descends Into Chaos as Crime Soars",
-      replacementHeadline: "City Reports Increase in Property Crime, Violent Crime Rates Steady",
+      replacementHeadline: "City Reports Increase in Property Crime; Violent Crime Rates Steady",
       status: "Pending",
       url: "https://fake.news/city-descends-into-chaos/"
     },
@@ -77,10 +75,9 @@ export default function Replacements() {
 
   return (
     <Page>
-      <div className="mt-10">
-        <div>
-          {sampleData.map(renderReplacement)}
-        </div>
+      <h1 className="text-2xl ml-11">Headline Replacements</h1>
+      <div className="mt-6">
+        {sampleData.map(renderReplacement)}
       </div>
     </Page>
   );
