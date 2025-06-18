e3ccac3e4487b620c12cd1a1efbe8fbb67c58580
Anil Dhurjaty <adhurjaty@gmail.com>
2025-06-17 22:00:20 -0700

    WIP: Refactor proposal

--- apps/webapp/src/Replacements.tsx
+++ apps/webapp/src/Replacements.tsx
@@ -1,5 +1,6 @@
 import { Fragment } from "react/jsx-runtime";
 import Page from "./components/Page";
+import Card from "./components/Card";
 
 type Replacement = {
   originalHeadline: string;
@@ -12,31 +13,93 @@ type ReplacementItemProps = {
   replacement: Replacement,
 }
 
+type HeadlineContentProps = {
+  label: string;
+  headline: string;
+  type: "original" | "replacement";
+  borderType: "start" | "end";
+}
+function HeadlineContent({ label, headline, type, borderType }: HeadlineContentProps) {
+  const styles = {
+    original: {
+      textColor: "text-orange-700",
+      bgColor: "bg-orange-50/50",
+      highlightColor: "bg-orange-100",
+      borderColor: "border-orange-400/50",
+    },
+    replacement: {
+      textColor:  "text-green-700",
+      bgColor: "bg-green-50/50",
+      highlightColor: "bg-green-100",
+      borderColor: "border-green-400/50",
+    }
+  }
+
+  const borderStyles = {
+    start: "md:border-r-0 rounded-t-md md:rounded-l-md md:rounded-tr-none",
+    end: "md:border-l-0 rounded-l-md md:rounded-r-md md:rounded-tl-none",
+  }
+
+  const {
+    textColor,
+    bgColor,
+    highlightColor,
+    borderColor
+  } = styles[type];
+
+  const borderStyle = borderStyles[borderType];
+
+  const boxStyle = [textColor, bgColor, borderColor].join(" ");
+   
+  return (
+    <>
+      <div
+        className={`text-xs pl-2 order-1 md:order-1 md:col-span-1 md:row-start-1 md:row-end-2 ${textColor}`}
+      >
+        {label.toUpperCase()}
+      </div>
+      <div
+        className={
+          `px-2 py-2 h-full text-left font-medium border whitespace-pre-line
+          break-words order-2 md:order-3 md:col-span-1 md:row-start-2 md:row-end-3
+          ${boxStyle} ${borderStyle}`
+        }
+      >
+        <span className={`${highlightColor} p-1 box-decoration-clone`}>
+          {headline}
+        </span>
+      </div>
+    </>
+  )
+}
+
 function ReplacementItem({replacement}: ReplacementItemProps) {
   return (
-    <div
-      className="col-span-2 mb-8 flex flex-col items-stretch px-2"
-    >
-      <div className="grid grid-cols-1 md:grid-cols-2 bg-white border border-gray-200 rounded-3xl shadow-lg px-4 md:px-8 py-4 items-center relative z-10 gap-y-2 md:gap-y-0" style={{ minHeight: '80px' }}>
-        {/* ORIGINAL label */}
-        <div className="text-xs pl-2 text-orange-700 order-1 md:order-1 md:col-span-1 md:row-start-1 md:row-end-2">ORIGINAL</div>
-        {/* ORIGINAL headline */}
-        <div className="px-2 py-2 h-full text-left font-medium text-orange-700 bg-orange-50/50 border md:border-r-0 border-orange-400/50 rounded-t-md md:rounded-l-md md:rounded-tr-none whitespace-pre-line break-words order-2 md:order-3 md:col-span-1 md:row-start-2 md:row-end-3">
-          <span className="bg-orange-100 p-1 box-decoration-clone">{replacement.originalHeadline}</span>
-        </div>
-        {/* REPLACEMENT label */}
-        <div className="text-xs pl-2 text-green-700 order-3 md:order-2 md:col-span-1 md:row-start-1 md:row-end-2 md:col-start-2">REPLACEMENT</div>
-        {/* REPLACEMENT headline */}
-        <div className="px-2 py-2 h-full font-semibold text-green-700 bg-green-50/50 border border-green-400/50 rounded-b-md md:rounded-r-md md:rounded-bl-none whitespace-pre-line break-words order-4 md:order-4 md:col-span-1 md:row-start-2 md:row-end-3 md:col-start-2">
-          <span className="bg-green-100 p-1 box-decoration-clone">{replacement.replacementHeadline}</span>
-        </div>
-        {/* Status and URL */}
-        <div className="flex flex-col sm:flex-row sm:items-center gap-2 my-2 text-sm text-gray-800 col-span-1 break-all order-5 md:order-5 md:col-span-2 md:row-start-3 md:row-end-4 md:my-2">
-          <div><span className="inline-block rounded-full bg-yellow-100 text-yellow-800 px-3 py-1 font-semibold text-sm shrink-0 text-left">{replacement.status}</span></div>
-          <a href={replacement.url} target="_blank" rel="noopener noreferrer" className="underline decoration-gray-400/50 hover:decoration-gray-800 transition-all text-left px-3 sm:px-1">{replacement.url}</a>
+    <Card>
+      <div
+        className="col-span-2 items-stretch px-2"
+      >
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-y-2 md:gap-y-0">
+          <HeadlineContent
+            label="Original"
+            headline={replacement.originalHeadline}
+            type="original"
+            borderType="start"
+          />
+          <HeadlineContent
+            label="Replacement"
+            headline={replacement.replacementHeadline}
+            type="replacement"
+            borderType="end"
+          />
+          {/* Status and URL */}
+          <div className="flex flex-col sm:flex-row sm:items-center gap-2 my-2 text-sm text-gray-800 col-span-1 break-all order-5 md:order-5 md:col-span-2 md:row-start-3 md:row-end-4 md:my-2">
+            <div><span className="inline-block rounded-full bg-yellow-100 text-yellow-800 px-3 py-1 font-semibold text-sm shrink-0 text-left">{replacement.status}</span></div>
+            <a href={replacement.url} target="_blank" rel="noopener noreferrer" className="underline decoration-gray-400/50 hover:decoration-gray-800 transition-all text-left px-3 sm:px-1">{replacement.url}</a>
+          </div>
         </div>
       </div>
-    </div>
+    </Card>
   );
 }
 
@@ -83,8 +146,8 @@ export default function Replacements() {
   return (
     <Page>
       <h1 className="text-2xl text-center">Headline Replacements</h1>
-      <div className="mt-6 sm:px-2 lg:px-10">
-        {sampleData.map((replacement, idx) => (
+      <div className="mt-6 gap-8 flex flex-col sm:px-2 lg:px-10">
+        {sampleData.map((replacement) => (
           <Fragment key={replacement.url} >
             <ReplacementItem replacement={replacement} />
           </Fragment>
