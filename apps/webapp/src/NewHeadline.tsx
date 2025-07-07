ee8580ee5794b02cdbf4007c6c5c50594031c361
James Harris <computersherpa@gmail.com>
2025-07-06 22:57:12 -0400

    Add citations section

--- apps/webapp/src/NewHeadline.tsx
+++ apps/webapp/src/NewHeadline.tsx
@@ -13,8 +13,9 @@ export default function NewHeadlinePage() {
             <div className="m-4">
                 <Card>
                     <h1 className="text-lg font-bold">Propose New Headline</h1>
+
                     <form className="flex flex-col">
-                        <div className="flex flex-row justify-between">
+                        <div className="mt-2 flex flex-row justify-between">
                             <label htmlFor="originalHeadline">Original headline</label>
                             <div className={originalHeadline.length > MAX_HEADLINE_LENGTH ? 'text-red-600' : ''}>
                                 {originalHeadline.length} / {MAX_HEADLINE_LENGTH}
@@ -26,7 +27,7 @@ export default function NewHeadlinePage() {
                             value={originalHeadline}
                             onChange={e => setOriginalHeadline(e.target.value)}
                         />
-                        <div className="flex flex-row justify-between">
+                        <div className="mt-2 flex flex-row justify-between">
                             <label htmlFor="replacementHeadline">Replacement headline</label>
                             <div className={replacementHeadline.length > MAX_HEADLINE_LENGTH ? 'text-red-600' : ''}>
                                 {replacementHeadline.length} / {MAX_HEADLINE_LENGTH}
@@ -39,6 +40,11 @@ export default function NewHeadlinePage() {
                             onChange={e => setReplacementHeadline(e.target.value)}
                         />
                     </form>
+
+					<section className="mt-2">
+						<h2 className="font-bold">Citations</h2>
+						<input type="text" placeholder="https://..." className="border border-gray-200 rounded-md p-2 w-full"/>
+					</section>
                 </Card>
             </div>
         </Page>
