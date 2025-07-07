491dce57ba2a173eee27e499331c78aa671a38b8
James Harris <computersherpa@gmail.com>
2025-07-06 23:24:49 -0400

    Group policy sidebar

--- apps/webapp/src/NewHeadline.tsx
+++ apps/webapp/src/NewHeadline.tsx
@@ -31,54 +31,64 @@ export default function NewHeadlinePage() {
 
     return (
         <Page>
-            <div className="m-4">
+            <div className="mx-auto p-4 max-w-4xl">
                 <Card>
-                    <h1 className="text-lg font-bold">Propose New Headline</h1>
-
-                    <form className="flex flex-col">
-                        <div className="mt-2 flex flex-row justify-between">
-                            <label htmlFor="originalHeadline">Original headline</label>
-                            <div className={originalHeadline.length > MAX_HEADLINE_LENGTH ? 'text-red-600' : ''}>
-                                {originalHeadline.length} / {MAX_HEADLINE_LENGTH}
+                    <div className="flex flex-row">
+                        <div className="flex-2/3">
+                            <h1 className="text-lg font-bold">Propose New Headline</h1>
+                            <form className="flex flex-col">
+                                <div className="mt-2 flex flex-row justify-between">
+                                    <label htmlFor="originalHeadline">Original headline</label>
+                                    <div className={originalHeadline.length > MAX_HEADLINE_LENGTH ? 'text-red-600' : ''}>
+                                        {originalHeadline.length} / {MAX_HEADLINE_LENGTH}
+                                    </div>
+                                </div>
+                                <textarea
+                                    id="originalHeadline"
+                                    className="border rounded-md p-2 bg-orange-50/50 text-orange-700 border-orange-400/50"
+                                    value={originalHeadline}
+                                    onChange={e => setOriginalHeadline(e.target.value)}
+                                />
+                                <div className="mt-2 flex flex-row justify-between">
+                                    <label htmlFor="replacementHeadline">Replacement headline</label>
+                                    <div className={replacementHeadline.length > MAX_HEADLINE_LENGTH ? 'text-red-600' : ''}>
+                                        {replacementHeadline.length} / {MAX_HEADLINE_LENGTH}
+                                    </div>
+                                </div>
+                                <textarea
+                                    id="replacementHeadline"
+                                    className="border rounded-md p-2 bg-green-50/50 text-green-700 border-green-400/50"
+                                    value={replacementHeadline}
+                                    onChange={e => setReplacementHeadline(e.target.value)}
+                                />
+                            </form>
+                            <section className="mt-2">
+                                <h2 className="font-bold">Citations</h2>
+                                {citations.map((citation, idx) => (
+                                    <input
+                                        key={idx}
+                                        type="text"
+                                        placeholder="https://..."
+                                        className="border border-gray-200 rounded-md p-2 w-full mb-2"
+                                        value={citation}
+                                        onChange={e => handleCitationChange(idx, e.target.value)}
+                                    />
+                                ))}
+                            </section>
+                            <div className="flex justify-between mt-2">
+                                <button className="px-3 py-1 rounded-md border border-gray-200">Cancel</button>
+                                <button className="px-3 py-1 rounded-md bg-blue-500 text-white font-bold">Save & Submit</button>
                             </div>
                         </div>
-                        <textarea
-                            id="originalHeadline"
-                            className="border rounded-md p-2 bg-orange-50/50 text-orange-700 border-orange-400/50"
-                            value={originalHeadline}
-                            onChange={e => setOriginalHeadline(e.target.value)}
-                        />
-                        <div className="mt-2 flex flex-row justify-between">
-                            <label htmlFor="replacementHeadline">Replacement headline</label>
-                            <div className={replacementHeadline.length > MAX_HEADLINE_LENGTH ? 'text-red-600' : ''}>
-                                {replacementHeadline.length} / {MAX_HEADLINE_LENGTH}
-                            </div>
-                        </div>
-                        <textarea
-                            id="replacementHeadline"
-                            className="border rounded-md p-2 bg-green-50/50 text-green-700 border-green-400/50"
-                            value={replacementHeadline}
-                            onChange={e => setReplacementHeadline(e.target.value)}
-                        />
-                    </form>
-
-                    <section className="mt-2">
-                        <h2 className="font-bold">Citations</h2>
-                        {citations.map((citation, idx) => (
-                            <input
-                                key={idx}
-                                type="text"
-                                placeholder="https://..."
-                                className="border border-gray-200 rounded-md p-2 w-full mb-2"
-                                value={citation}
-                                onChange={e => handleCitationChange(idx, e.target.value)}
-                            />
-                        ))}
-                    </section>
-					<div className="flex justify-between mt-2">
-						 <button className="px-3 py-1 rounded-md border border-gray-200">Cancel</button>
-						 <button className="px-3 py-1 rounded-md bg-blue-500 text-white font-bold">Save & Submit</button>
-					</div>
+                        <aside className="pl-4 flex-1/3 shrink-0">
+                            <h2 className="font-bold my-2">Group policy tips</h2>
+                            <ul className="list-disc pl-6 flex flex-col gap-1">
+                                <li>Strive for accuracy</li>
+                                <li>Avoid loaded language</li>
+                                <li>Provide supporting evidence</li>
+                            </ul>
+                        </aside>
+                    </div>
                 </Card>
             </div>
         </Page>
